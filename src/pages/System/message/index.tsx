c8b26ff34633c49f3b788bdc1b5f36e0670c5f01
ShaunZh <308826842@qq.com>
2020-02-10 23:20:22 +0800

    update: 完成消息管理页面

--- src/pages/System/message/index.tsx
+++ src/pages/System/message/index.tsx
@@ -1,7 +1,11 @@
 import { PageHeaderWrapper } from '@ant-design/pro-layout';
 import React, { useState, useEffect } from 'react';
-import { Spin } from 'antd';
+import { Spin, Select, Button, Input, Row, Col } from 'antd';
 import styles from './index.less';
+import TableBasic from './components/TableBasic';
+
+const { Option } = Select;
+const { Search } = Input;
 
 export default () => {
   const [loading, setLoading] = useState<boolean>(true);
@@ -10,11 +14,41 @@ export default () => {
       setLoading(false);
     }, 500);
   }, []);
+  const handleChange = (e: string) => {
+    console.log('e', e);
+  };
   return (
-    <PageHeaderWrapper content="这是一个新页面，从这里进行开发！" className={styles.main}>
-      <div style={{ paddingTop: 100, textAlign: 'center' }}>
+    <PageHeaderWrapper className={styles.main}>
+      <Row className="header" type="flex" align="middle" justify="space-between">
+        <Col className="left">
+          <span className="u-name">消息状态: </span>
+          <Select
+            defaultValue="lucy"
+            style={{ width: 240, height: 34, marginRight: 20, marginLeft: 10 }}
+            onChange={handleChange}
+          >
+            <Option value="jack">Jack</Option>
+            <Option value="lucy">Lucy</Option>
+          </Select>
+          <Button type="primary">新增消息</Button>
+        </Col>
+        <Col className="right">
+          <Search
+            placeholder="输入搜索内容"
+            onSearch={value => console.log(value)}
+            style={{ width: 200 }}
+          />
+        </Col>
+      </Row>
+      <div
+        style={{
+          paddingTop: 30,
+          textAlign: 'center',
+        }}
+      >
         <Spin spinning={loading} size="large"></Spin>
       </div>
+      <TableBasic />
     </PageHeaderWrapper>
   );
 };
