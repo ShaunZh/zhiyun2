73e36896eafbfbddfb7e30487f59951d5b129d66
ShaunZh <308826842@qq.com>
2020-02-13 20:57:44 +0800

    add: mock api

--- mock/system.ts
+++ mock/system.ts
@@ -1,11 +1,11 @@
-import mockjs from 'mockjs';
+import mockjs, { mock } from 'mockjs';
 
 export default {
   'POST /api/system/operator/list': (req, res) => {
     res.send(
       mockjs.mock({
         page: {
-          total: '@integer(0, 100)',
+          total: 100,
         },
         'data|10': [
           {
@@ -28,5 +28,33 @@ export default {
         value: '@id',
       },
     ],
+  }),
+  'POST /api/system/operator/create': {
+    code: 200,
+  },
+
+  'POST /api/system/operator/roleOptions': mockjs.mock({
+    code: 200,
+    'result|1-5': [
+      {
+        label: '@first',
+        value: '@id',
+      },
+    ],
+  }),
+  'POST /api/system/operator/detail': mockjs.mock({
+    code: 200,
+    result: {
+      number: '@id',
+      account: '@id',
+      operator: '@first',
+      mobile: '1212121',
+      'role|1': ['管理员', '超级管理员'],
+      'status|1': ['已启用', '已禁用'],
+      time: '@datetime',
+    },
+  }),
+  'POST /api/system/operator/update': mockjs.mock({
+    code: 200,
   }),
 };
