4782162aea251409cbc8f617d7786a87578bfd1d
ShaunZh <308826842@qq.com>
2020-02-14 14:29:24 +0800

    update: mock api

--- mock/system.ts
+++ mock/system.ts
@@ -1,25 +1,28 @@
-import mockjs, { mock } from 'mockjs';
+import mockjs from 'mockjs';
+import { Request, Response } from 'express';
 
 export default {
-  'POST /api/system/operator/list': (req, res) => {
-    res.send(
-      mockjs.mock({
-        page: {
-          total: 100,
-        },
-        'data|10': [
-          {
-            number: '@id',
-            account: '@id',
-            operator: '@first',
-            mobile: '1212121',
-            'role|1': ['管理员', '超级管理员'],
-            'status|1': ['已启用', '已禁用'],
-            time: '@datetime',
+  'POST /api/system/operator/list': (req: any, res: any) => {
+    setTimeout(() => {
+      res.send(
+        mockjs.mock({
+          page: {
+            total: 100,
           },
-        ],
-      }),
-    );
+          'data|10': [
+            {
+              number: '@id',
+              account: '@id',
+              operator: '@first',
+              mobile: '1212121',
+              'role|1': ['管理员', '超级管理员'],
+              'status|1': ['已启用', '已禁用'],
+              time: '@datetime',
+            },
+          ],
+        }),
+      );
+    }, 3000);
   },
   'POST /api/system/operator/statusOptions': mockjs.mock({
     'result|1-5': [
@@ -29,7 +32,7 @@ export default {
       },
     ],
   }),
-  'POST /api/system/operator/create': (req, res) => {
+  'POST /api/system/operator/create': (req: any, res: any) => {
     setTimeout(
       () =>
         res.send({
@@ -63,7 +66,7 @@ export default {
   'POST /api/system/operator/update': mockjs.mock({
     code: 200,
   }),
-  'POST /api/system/operator/remove': (req, res) => {
+  'POST /api/system/operator/remove': (req: Request, res: Response) => {
     setTimeout(() => {
       res.send({
         code: 200,
