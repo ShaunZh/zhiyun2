9f1d98e94a6ded2b4f976d9e682da51dea250007
ShaunZh <308826842@qq.com>
2020-02-28 11:21:58 +0800

    add: common component - Radios

---
+++ src/components/Radios/index.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Radio } from 'antd';
+import { RadioChangeEvent } from 'antd/es/radio';
+
+interface IProps {
+  defaultValue: string;
+  radiosList: Array<ISelectOption>;
+  handleChange: (v: string) => void;
+}
+
+const Radios: React.FC<IProps> = props => {
+  const radiosButton = props.radiosList.map(item => (
+    <Radio.Button value={item.value} key={item.label}>
+      {item.label}
+    </Radio.Button>
+  ));
+  const handleChange = (e: RadioChangeEvent) => {
+    props.handleChange(e.target.value);
+  };
+
+  return (
+    <Radio.Group defaultValue={props.defaultValue} buttonStyle="solid" onChange={handleChange}>
+      {radiosButton}
+    </Radio.Group>
+  );
+};
+
+export default Radios;
