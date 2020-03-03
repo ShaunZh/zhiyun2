4b76c2b6d2af52ba0869403c71049827609f2e4f
ShaunZh <308826842@qq.com>
2020-03-03 16:16:52 +0800

    add: consts文件定义常量

---
+++ src/pages/course/course/index/consts.ts
@@ -0,0 +1,12 @@
+/*
+ * @Description: 常量
+ * @Author: Hexon
+ * @Date: 2020-03-02 16:26:03
+ * @LastEditors: Hexon
+ * @LastEditTime: 2020-03-02 16:29:57
+ */
+export default {
+  session: {
+    listQuery: 'session-temp-list-query',
+  },
+};
